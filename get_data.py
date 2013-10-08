a47b0f72d73ab4de4804b1b37f56f111950a842d
Zachary M. Jones <zmj@zmjones.com>
2013-10-07 22:15:02 -0400

    scrapes population data, adds new plots

--- get_data.py
+++ get_data.py
@@ -4,8 +4,6 @@ import requests, re
 from bs4 import BeautifulSoup
 import pandas as pd
 
-base_url = 'http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
-
 def read_table(url):
    soup = BeautifulSoup(requests.get(url).content)
    return soup.find(lambda tag:tag.name == 'table')
@@ -14,7 +12,7 @@ def parse_table(url):
    table = read_table(url)
    return [[td.get_text(strip=True)
             if td.find("a") is None 
-            else base_url + td.find("a")["href"] 
+            else url + td.find("a")["href"] 
             for td in tr.find_all("td") if td.get_text(strip=True) is not u""] 
             for tr in table.find_all("tr")]
 
@@ -30,10 +28,12 @@ def parse_statement(url):
 
 columns = ['execution', 'offender_information', 'last_statement', 'last_name',
            'first_name', 'tdcj_number', 'age', 'date', 'race', 'county']
-data = parse_table(base_url)
+data = parse_table('http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html')
 del data[0]
 data = pd.DataFrame(data, columns = columns)
 data.ix[:, (1,2)] = data.ix[:, (1,2)].applymap(lambda x: re.sub('dr_executed_offenders.html', '', x))
 data['statement_text'] = data['last_statement'].map(lambda x: parse_statement(x))
 data.to_csv('statement_data.csv', index=False, encoding='utf-8')
 
+data = parse_table('https://www.tsl.state.tx.us/ref/abouttx/popcnty2010-11.html')
+pd.DataFrame(data).to_csv('population_data.csv', index=False, encoding='utf-8')
